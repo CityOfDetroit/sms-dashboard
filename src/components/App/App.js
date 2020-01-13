b01654f404a2dee5d14d0c4504418752c3ae58ca
Edgar Montes <jedgar1mx@gmail.com>
2020-01-13 17:46:12 -0500

    Adding dynamic load of services.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,57 @@
 import React, { useState }from 'react';
 import './App.scss';
+import data from '../../data/App.data.json';
 
 function App() {
     // Declare a new state variable, which we'll call "count"
-  const [form, setForm] = useState();
+  const [service, setService] = useState(null);
+  const [user, setUser] = useState({'id': 'edgar', 'permissions': ['elections']});
+
+  const buildServices = () => {
+    const markup = data.services.map((service) =>
+        (user.permissions.includes(service.id)) ? <option key={service.id} value={service.id}>{service.name}</option> : ''
+    );
+    return markup;
+  }
+
+  const handleServiceChange = (ev) => {
+    setService(ev.target.value);
+  }
+
+  const buildGroupFilters = () => {
+
+    const markup = <fieldset>
+    <label htmlFor="group" className="required-field">Group</label>
+    <select id="group" required>
+    <option value="">--Please choose a group--</option>
+    <option value="all">All</option>
+    <option value="list-1">List 1</option>
+    <option value="list-2">List 2</option>
+    </select>
+  </fieldset>;
+  return markup;
+  }
+
+  const buildStaticFilters = () => {
+
+  }
+
+  const buildDynamicFilters = () => {
+    
+  }
 
   return (
     <section className="App">
+      <p>{document.title}</p>
      <form>
         <fieldset>
-          <label htmlFor="service" className="required-field">Service</label>
-          <select id="group" required>
+          <label htmlFor="services" className="required-field">Service</label>
+          <select id="services" onChange={handleServiceChange} required>
           <option value="">--Please choose a service--</option>
-          <option value="elections">Elections</option>
-          <option value="waste">DPW Waste Pick up</option>
-          <option value="census">Census</option>
+          {buildServices()}
           </select>
         </fieldset>
-        <fieldset>
-          <label htmlFor="group" className="required-field">Group</label>
-          <select id="group" required>
-          <option value="">--Please choose a group--</option>
-          <option value="all">All</option>
-          <option value="list-1">List 1</option>
-          <option value="list-2">List 2</option>
-          </select>
-        </fieldset>
+        {(service) ? buildGroupFilters() : ""}
         <fieldset>
           <label htmlFor="en-msg" className="required-field">Message</label>
           <textarea id="en-msg" type="text" required></textarea>
