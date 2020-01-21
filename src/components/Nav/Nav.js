e72c29b61a548edf575d295e75a4ad7022790a33
Edgar Montes <jedgar1mx@gmail.com>
2020-01-21 18:46:53 -0500

    Split App into more granular components.

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React, { useState }from 'react';
+import './Nav.scss';
+
+function Nav() {
+
+  return (
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Message</li>
+          <li>Number</li>
+        </ul>
+      </nav>
+  );
+}
+
+export default Nav;
+
