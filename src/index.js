cb5c0ab2f250e7cd36dd1a89bb869c1ff4ae132c
Edgar Montes <jedgar1mx@gmail.com>
2020-01-28 12:13:33 -0500

    Adding message.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-(drupalSettings.intranet.intranetJs.appAccess == 'true') ? ReactDOM.render(<App />, document.getElementById('root')) : '';
+(drupalSettings.intranet.intranetJs.appAccess == 'true') ? ReactDOM.render(<App />, document.getElementById('root')) : document.getElementById('root').innerHTML = <h1>Permission Denied.</h1>;
