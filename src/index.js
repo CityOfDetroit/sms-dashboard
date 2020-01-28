affd51774807cae526b118ff198ed0a77d919476
Edgar Montes <jedgar1mx@gmail.com>
2020-01-28 12:08:51 -0500

    adding permission control.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+(drupalSettings.intranet.intranetJs.appAccess == 'true') ? ReactDOM.render(<App />, document.getElementById('root')) : '';
